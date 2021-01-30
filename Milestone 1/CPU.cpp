92d9a7c8932538704d60e5d664dae197e66fc0c0
Enlightened-Havok <erik_manley@hotmail.com>
2021-01-30 16:38:45 -0700

    Made a quick draft of the opening GUI so that whoever wants to use the CPU as they build it can do so. NOTE: this version does not include testing for valid input or exception handling.

---
+++ Milestone 1/CPU.cpp
@@ -0,0 +1,49 @@
+#include <map>
+using namespace std;
+/*
+void CPU(map inputMap){
+    //create loop that takes input and performs function based on input
+    for(int i = 0; i < 100; i++){
+        //pull out first two characters of each input to gather instruction, store remaining numbers in another var.
+        //switch/case for each instruction
+        switch(input){
+            case 10:
+                Read();
+                break;s
+            case 11:
+                Write();
+                break;
+            case 20: 
+                Load();
+                break;
+            case 21: 
+                Store();
+                break;
+            case 30:
+                Add();
+                break;
+            case 31:
+                Subtract();
+                break;
+            case 32:
+                Divide();
+                break;
+            case 33: 
+                Multiply();
+                break;
+            case 40: 
+                Branch();
+                break;
+            case 41: 
+                BranchNeg();
+                break;
+            case 42: 
+                BranchZero();
+                break;
+            case 43: 
+                Halt();
+                break;
+        }
+    }
+
+}*/
--- Milestone 1/GUI.cpp
+++ Milestone 1/GUI.cpp
@@ -1,60 +1,49 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include "CPU.cpp"
 using namespace std;
 
 void GUI(){
 //post welcome message and instructions
-    string welcome = "blah";
-    cout << welcome;
+    cout << "**** Welcome to UV SIM! ****\n" <<
+        "**** Please enter your program one instruction ****\n" <<
+        "**** (or data word) at a time into the input   ****\n" <<
+        "**** text field. I will display the location   ****\n" <<
+        "**** and a question mark (?). You then         ****\n" <<
+        "**** type the word for that location. Enter    ****\n" <<
+        "**** -99999 to stop entering your program      ****" << endl;
+
+    //initialize map
+    map<size_t,string> inputMap;
 
     for(int i = 0; i < 100; i++){
+        string line;
+        if (i < 10) line = "0" + to_string(i);
+        else line = i;
+        cout << line + " ? ";
+
+        //gather user input
         string uInput;
         cin >> uInput;
-        int test = parseInt(uInput[0] + uInput[1], 10);
+        //stop gathering if user types -99999
+        if (uInput == "-99999") break;
+        
+        //place user input into map
+        inputMap[i] = uInput;
     }
 
-//create loop that takes input and performs function based on input
-    for(int i = 0; i < 100; i++){
-        //pull out first two characters of each input to gather instruction, store remaining numbers in another var.
-        //switch/case for each instruction
-        switch(test){
-            case 10:
-                Read();
-                break;
-            case 11:
-                Write();
-                break;
-            case 20: 
-                Load();
-                break;
-            case 21: 
-                Store();
-                break;
-            case 30:
-                Add();
-                break;
-            case 31:
-                Subtract();
-                break;
-            case 32:
-                Divide();
-                break;
-            case 33: 
-                Multiply();
-                break;
-            case 40: 
-                Branch();
-                break;
-            case 41: 
-                BranchNeg();
-                break;
-            case 42: 
-                BranchZero();
-                break;
-            case 43: 
-                Halt();
-                break;
-        }
+    /* test output
+    for (auto input : inputMap){
+        cout << input.first << ": " << input.second << endl;
     }
 
+    int end;
+    cin >> end;
+    */
+
+   /* pass to CPU
+   CPU(inputMap);
+   */
 }
 
