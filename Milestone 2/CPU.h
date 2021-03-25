eaa1609c8a8e9b6230b6f28ae5fb38df6791bb92
santisam95 <santisam.95@outlook.com>
2021-03-24 18:55:08 -0600

    adding to bring my branch up to date with new code

--- Milestone 2/CPU.h
+++ Milestone 2/CPU.h
@@ -21,22 +21,46 @@ public:
     int currentPage;
 
     //functions
-    bool hasMemory();
+    bool hasMemory(){
+        for(pair<size_t, string> element : inputMap){ ///location of map declaration needs to be fixed.
+            if(element.first != NULL){
+                return true;
+            }
+        }
+    };
+
+    ///function needs work
     void updateMenu(int); ///used to tell the VIEW to display information if needed.
-    void loadMemory(MEMORY);  ///need to find a way to handle this
+    //*
+    // edit mode needs more
+    // if edit mode is being requested, check if memory has anything in it, and then send that
+    // to be displayed
+    // edit mode is page 6.
+    // *//
+    ///view.display(int) which is the page to load.
+
+    ///function on hold until how data in CONTROLLER is handled.
+    void loadMemory(MEMORY){
+
+    };
+    ///this is going to be coming from a file.
+    ///controller will be telling me which one to load.
 
     void updateMemory(string opcode, int memLocation){
 
-    }
+        Memory.add(opcode, memLocation); ///this has an error on MEMORY undeclared identifier
 
+    };
 };
 //SUBCLASS MEMORY
 class MEMORY{
 
 public:
     //Constructor
-    MEMORY(map){};  ///what is the purpose of this constructor and is it needed also needs fixed
+    MEMORY(){
 
+        map<size_t, string> inputMap;
+    };
     //variables
     map<size_t, string> inputMap;
     bool done = false;
@@ -45,8 +69,32 @@ public:
     string line;
 
     //functions
-    void remove(int);
-    void add(string, int);
+    void remove(int){
+
+        for(pair<size_t, string> element : inputMap){
+            if(element.second == int){ ///typecast issue
+                element.first = ""; ///typecast issue
+            }
+        }
+    };
+
+    //adds a line of user input into the memory
+    void add(string uInput, int memLocation){
+        for(pair<size_t, string> element : inputMap){
+            ///accessing KEY from element
+            if(element.first == ""){ ///typecast issue
+                element.first = uInput; ///typecast issue
+                element.second = memLocation;
+            }
+            else{
+                ///some error handling if we are trying to add to a location that already has something there.
+            }
+        }
+    };
+
+    map getter(){
+        return map;
+    };
 
 
 
@@ -99,13 +147,7 @@ public:
                 for (size_t j = 0; j < diff; j++)
                     line.insert(1, "0");
             }
-
-            //Memory location contains data
             else {
-                //cout << "Sign: " << sign << endl;
-
-                //==========================================================
-                //   Gets the instruction code to be used in the switch
                 opcode = abs(stoi(line.substr(1, 2))); //Extract opcode substring
                 //==========================================================
                 //   Gets the data code to be used in the switch
